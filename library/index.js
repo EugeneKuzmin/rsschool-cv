00fb8475fc18e0fd1fef744feb6147ab99d88d11
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-08-06 17:59:26 +0300

    feat: mobile menu developed

--- library/index.js
+++ library/index.js
@@ -1,1 +1,19 @@
+const navigation = document.getElementById('navigation')
+const hamburger = document.querySelector('.hamburger')
+hamburger.addEventListener('click',switchNavigation)
+
+function switchNavigation(){
+
+    if (navigation.getAttribute("data-visible") === "false") {
+        navigation.setAttribute("data-visible", true);
+        hamburger.setAttribute("aria-expanded", true);
+    } else {
+        navigation.setAttribute("data-visible", false);
+        hamburger.setAttribute("aria-expanded", false);
+    }
+    
+
+}
+
+
 console.log("Общая оценка 94\n Вёрстка валидная +10\n Вёрстка семантическая +16\n Вёрстка соответствует макету +50\n блок <header> +8\n секция Welcome +4.\n секция About +6\n секция Favorites +6\n секция CoffeShop +6.\n секция Contacts +6\n секция LibraryCard +8/блок <footer> +6\n Общие требования к верстке +18")
