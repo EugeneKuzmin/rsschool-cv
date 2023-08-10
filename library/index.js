08443a9b6a7e498fc6a278ae3e223542f3aa90f7
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-08-10 13:25:42 +0300

    feat:main layout desctop fitted

--- library/index.js
+++ library/index.js
@@ -1,6 +1,10 @@
-const navigation = document.getElementById('navigation')
+const navigation = document.querySelector('.navigation')
 const hamburger = document.querySelector('.hamburger')
 hamburger.addEventListener('click',switchNavigation)
+
+
+const closeCross = document.querySelector('.close-cross')
+closeCross.addEventListener('click', switchNavigation)
 
 function switchNavigation(){
 
@@ -11,9 +15,9 @@ function switchNavigation(){
         navigation.setAttribute("data-visible", false);
         hamburger.setAttribute("aria-expanded", false);
     }
-    
 
 }
+
 
 
 console.log("Общая оценка 94\n Вёрстка валидная +10\n Вёрстка семантическая +16\n Вёрстка соответствует макету +50\n блок <header> +8\n секция Welcome +4.\n секция About +6\n секция Favorites +6\n секция CoffeShop +6.\n секция Contacts +6\n секция LibraryCard +8/блок <footer> +6\n Общие требования к верстке +18")
